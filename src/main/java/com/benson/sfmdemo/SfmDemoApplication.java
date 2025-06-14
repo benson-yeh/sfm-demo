c98f672bb0f64a1c770c9abcd073452835865770
benson.yeh <hh963369@gmail.com>
2025-06-14 08:41:33 +0800

    format whole project with spotlessApply

--- src/main/java/com/benson/sfmdemo/SfmDemoApplication.java
+++ src/main/java/com/benson/sfmdemo/SfmDemoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SfmDemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SfmDemoApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SfmDemoApplication.class, args);
+  }
 }
