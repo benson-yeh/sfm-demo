4f771df56c43ceef38eb7ab21c38b1e1c2d8e14e
benson.yeh <hh963369@gmail.com>
2025-06-14 08:39:44 +0800

    init

---
+++ src/main/java/com/benson/sfmdemo/SfmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.benson.sfmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfmDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benson/sfmdemo/SfmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benson.sfmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
